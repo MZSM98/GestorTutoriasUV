380404a212b816f7d82d766cbc8c909db4eb36be
MZSM98 <marcoszenon98@gmail.com>
2025-12-05 15:00:52 -0600

    Creación del repositorio

---
+++ src/main/java/com/gtuv/controlador/FormularioUsuarioController.java
@@ -0,0 +1,24 @@
+package com.gtuv.controlador;
+
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class FormularioUsuarioController implements Initializable {
+   
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+}
