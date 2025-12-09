f33712e0bf23aedb1ccf869fbe3f43ded5643e66
naess-einarr <jorgecobos98@icloud.com>
2025-12-09 02:22:26 -0600

    Feat FormularioFechaTutoria

--- src/main/java/com/gtuv/controlador/GestionFechaTutoriaController.java
+++ src/main/java/com/gtuv/controlador/GestionFechaTutoriaController.java
@@ -2,9 +2,24 @@ package com.gtuv.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class GestionFechaTutoriaController implements Initializable {
+
+    @FXML
+    private TableView<?> tblPlanTutorias;
+    @FXML
+    private TableColumn<?, ?> colNoSesion;
+    @FXML
+    private TableColumn<?, ?> colFechaProgramada;
+    @FXML
+    private TableColumn<?, ?> colEstado;
+    @FXML
+    private TextField txtPeriodoEscolar;
    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
